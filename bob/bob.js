a4b0488e545d404decc74847c5784c960e468894
Horacio Borrego <hborrego365@gmail.com>
2020-11-24 11:44:42 -0700

    Add additional code to pass tests

---
+++ bob/bob.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+// 'Sure' if a question
+// 'Whoa', chill out!' if all CAPS
+// 'Calm down, I know what I'm doing!' if you yell a question ?!
+// 'Fine. Be that way!'  if you address w/o saying anything
+// 'Whatever.' to anything else
+export const hey = (message) => {
+  if (message.toUpperCase()) {
+    return 'Whoa, chill out!'
+  }
+};
